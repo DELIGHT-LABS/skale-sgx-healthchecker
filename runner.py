b73e5710320bda9f4b0a4c86842938fa273454f8
Bryan Rhee <psy2848048@gmail.com>
2023-07-18 16:39:22 +0900

    enhence: added journal logger

--- runner.py
+++ runner.py
@@ -1,11 +1,29 @@
 from flask import Flask, make_response, json as flaskJson
 import requests
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter(fmt="%(asctime)s %(name)s.%(levelname)s: %(message)s", datefmt="%Y.%m.%d %H:%M:%S")
+
+handler = logging.StreamHandler(stream=sys.stdout)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 app = Flask(__name__)
+app.logger = logger
 
 @app.route('/health')
 def sgx_health():
-    ret = _post("http://localhost:1030", '{"jsonrpc":"2.0","id":1,"method":"getServerConfiguration","params":{}}')
+    try:
+        ret = _post("http://localhost:1030", '{"jsonrpc":"2.0","id":1,"method":"getServerConfiguration","params":{}}')
+        app.logger.info("well monitored...")
+    except Exception as e:
+        app.logger.error(e)
+        return make_response(flaskJson.jsonify(errorMsg=e), 400)
+
     return make_response(flaskJson.jsonify(**ret), 200)
 
 class HttpError(Exception):
@@ -16,7 +34,7 @@ class HttpError(Exception):
 def _post(url, payload=None):
     headers = {}
     headers['Content-Type'] = 'application/json'
-    response = requests.post(url, headers=headers, data=payload)
+    response = requests.post(url, headers=headers, data=payload, timeout=10)
     return _get_response(response)
 
 def _get_response(response):
@@ -28,5 +46,7 @@ def _get_response(response):
 
 if __name__ == "__main__":
     from gevent.pywsgi import WSGIServer
+
     http_server = WSGIServer(('0.0.0.0', 5000), app)
+    app.logger.info("Monitoring server started...")
     http_server.serve_forever()
